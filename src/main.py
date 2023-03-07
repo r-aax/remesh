4570ec94b7b6b3d9fa2630718f5625f8ac50c163
alexmeshr <mao2501@rambler.ru>
2023-03-07 20:21:29 +0300

    new reduce edges in Tong

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     #case = '../cases/naca/naca_t12.dat'
     #case = '../cases/blender_custom_meshes/one_hole_new.dat'
     #case = '../cases/blender_custom_meshes/plate.dat'
-    case = '../cases/bunny_fixed.dat'
+    case = '../cases/bunny.dat'
     #case = '../cases/pseudogrids/ex1.dat'
     RemesherTong().remesh(case, '../res_tong.dat')
     RemesherIsotropic().remesh(case, '../res_isotropic.dat')
